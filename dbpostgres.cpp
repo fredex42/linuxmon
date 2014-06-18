f75992c9dbad982080274cbc76d689d8b6e3866a
Andy Gallagher <fredex42@users.noreply.github.com>
2014-06-18 15:40:43 +0000

    Start of database integrtion. Requires libpqxx

---
+++ dbpostgres.cpp
@@ -0,0 +1,57 @@
+/*
+ * dbpostgres.cpp
+ *
+ *  Created on: 18 Jun 2014
+ *      Author: andy
+ */
+
+#include "dbpostgres.h"
+#include <stdarg.h>
+
+using namespace pqxx;
+
+db_postgres::~db_postgres() {
+	// TODO Auto-generated destructor stub
+	if(needs_commit) this->commit();
+	if(conn) delete conn;
+}
+
+void db_postgres::begin() {
+}
+
+db_postgres::db_postgres(const string& host, const string& port,const string& user,
+		const string& pass, const string& dbname) {
+string connectionString;
+
+connectionString="host="+host+" username="+user+" password="+pass+" dbname="+dbname;
+if(port!=""){
+	connectionString+=" port="+port;
+}
+
+cerr << "DEBUG: postgres connection string='"<<connectionString<<"'\n";
+
+conn=new connection(connectionString);
+needs_commit=false;
+}
+
+db_postgres::db_postgres(const string& dbname) {
+conn=new connection("dbname="+dbname);
+needs_commit=false;
+}
+
+db_postgres::db_postgres() {
+conn=NULL;
+needs_commit=false;
+}
+
+void db_postgres::commit() {
+}
+
+void db_postgres::doSQL(const string &sql,...) {
+va_list args;
+
+va_start(args,sql);
+
+va_end(args);
+
+}
--- main.cpp
+++ main.cpp
@@ -6,12 +6,73 @@
  */
 
 #include <iostream>
+#include <map>
+#include <string>
+#include <string.h>
+#include <getopt.h>
+
 #include "procstat.h"
 #include "procmeminfo.h"
 #include "procloadavg.h"
 
+using namespace std;
+
+std::map<std::string,std::string> opts;
+typedef std::map<std::string,std::string>::iterator options_iterator;
+
+int parse_options(int argc,char **argv)
+{
+/*
+int c;
+static struct option long_options[]={
+		{ "dbhost", required_argument,0,'h' },
+		{ "dbuser", required_argument,0,'u' },
+		{ "dbpass", required_argument,0,'p' },
+		{ "dbport", required_argument,0,'n' }
+};
+
+
+while(1){
+	int option_index=0;
+	c=getopt_long(argc,argv,"hupn",long_options,&option_index);
+
+}
+*/
+string argname,value;
+int n,nopts=0;
+/* arg 0 is the program name */
+
+for(n=1;n<argc;n+=2){
+	if(n+1>argc) break;
+	if(strlen(argv[n])<2) continue;
+//	cerr << "debug: got arg " << argv[n] << "\n";
+	if(argv[n][0]=='-'){
+		if(argv[n][1]=='-'){
+			argname=(char *)&argv[n][2];
+		} else {
+			argname=(char *)&argv[n][1];
+		}
+		value=argv[n+1];
+		opts[argname]=value;
+		++nopts;
+	} else {
+		continue;
+	}
+}
+return nopts;
+}
+
 int main(int argc,char *argv[])
 {
+int n_opts=parse_options(argc,argv);
+
+/*
+cerr << "debug: " << n_opts << " options provided: " << endl;
+for(options_iterator i=opts.begin();i!=opts.end();++i){
+	cerr << "\t" << i->first << "=" << i->second << endl;
+}
+*/
+
 class procstat stat=procstat();
 
 stat.update();
